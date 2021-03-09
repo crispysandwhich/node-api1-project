b9da51146ace2c12ec8f9f346cf04ff05a20a94d
de_gentlem4n <th3g3n3lm4n@gmail.com>
2021-03-09 11:26:45 -0500

    get request

--- api/server.js
+++ api/server.js
@@ -7,11 +7,19 @@ const server = express()
 server.use(express.json())
 
 
-// [POST REQUEST]
+// [GET REQUEST]
+server.get('/api/users', (req,res) => {
+  User.find()
+    .then(users => {
+      console.log(users)
+      res.status(200).json(users)
+    })
+    .catch(err => {res.status(500).json({message: err.message})})
+})
 
 // [get] everything
 server.use('*', (req,res) => {
-  res.status(200).json({message: 'what islife life'})
+  res.status(404).json({message: 'what islife life'})
 })
 
 
